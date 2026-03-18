d2219960fba0e11c3cce7271ba8f583d4a73ec28
Varun85535 <kvvarunreddy@gmail.com>
2026-03-18 05:52:14 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "instance" {
+count = 1
+ami = "ami-02dfbd4ff395f2a1b"
+instance_type = "t2.micro"
+tags = {
+Name = "myinstance"
+}
+}
