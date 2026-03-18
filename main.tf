e0bd4a5cd7908af0887bc18059b7ecfefa6aa62f
Varun85535 <kvvarunreddy@gmail.com>
2026-03-18 07:45:16 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
 region = "us-east-1"
 }
-
+resource "aws_s3_bucket" "bucket" {
+bucket = "terra-bucket-varun85535"
+}
 resource "aws_instance" "instance" {
-count = 3
+count = 1
 ami = "ami-02dfbd4ff395f2a1b"
 instance_type = "t2.micro"
 tags = {
